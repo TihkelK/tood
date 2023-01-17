742b9e614ae0183678f4793f0a4c2ba65bdd290b
TihkelK <116258789+TihkelK@users.noreply.github.com>
2023-01-17 15:28:16 +0200

    Add files via upload

---
+++ TKyl5.py
@@ -0,0 +1,95 @@
+from tkinter import *
+from tkinter import ttk
+
+def arvuta():
+    hind = float(sisestus.get())
+    kmaks = var.get()
+    arvutus = hind*kmaks
+    tekst2.config(text=arvutus)
+    arvutus2 = hind+arvutus
+    tekst3.config(text=arvutus2)
+    print(arvutus)
+   
+aken = Tk()
+aken.resizable(0, 0)
+aken.title("Käibemaksukalkolaator")
+aken.geometry("300x300")
+
+sisestus = Entry(aken)
+sisestus.grid(row=1, column=1)
+
+tekst = Label(aken,
+              text="Hind käibemaksuta ",
+              font="Tahoma 12",
+              padx=2,
+              pady=2)
+tekst.grid(row=1, column=0, columnspan=1)
+
+tekst.grid(row=1,column=0)
+
+
+tekst = Label(aken,
+              text="Käibemaksumäär ",
+              font="Tahoma 12",
+              padx=2,
+              pady=2)
+tekst.grid(row=2, column=0, columnspan=1)
+
+var = DoubleVar()
+valikukast = Radiobutton(aken,text="9%", variable=var, value=0.09)
+valikukast.grid(row=3, column=1)
+valikukast = Radiobutton(aken,text="20%", variable=var, value=0.2)
+valikukast.grid(row=4, column=1)
+
+joon = Label(aken,
+              text="__________________________________",
+              font="Tahoma 12",
+              padx=2,
+              pady=2)
+joon.grid(row=5, column=0, columnspan=2)
+
+tekst = Label(aken,
+              text="Käibemaks ",
+              font="Tahoma 12",
+              padx=2,
+              pady=2)
+
+tekst.grid(row=6, column=0, sticky="w")
+
+tekst2 = Label(aken,
+              text="0€ ",
+              font="Tahoma 12",
+              padx=2,
+              pady=2)
+
+tekst2.grid(row=6, column=1, columnspan=3)
+
+
+tekst = Label(aken,
+              text="Hind Käibemaksuga ",
+              font="Tahoma 12",
+              padx=2,
+              pady=2)
+
+tekst.grid(row=7, column=0, sticky="w")
+
+tekst3 = Label(aken,
+              text="0€",
+              font="Tahoma 12",
+              padx=2,
+              pady=2)
+
+tekst3.grid(row=7, column=1, columnspan=3)
+
+nupp1 = Button(aken, text="=", width=10, command=arvuta)
+nupp1.grid(row=8, column=1)
+
+
+
+
+
+
+
+
+
+aken.mainloop()
